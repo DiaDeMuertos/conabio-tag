2f1469472d5f3c25b666dd40455d2655565fd32b
DiaDeMuertos <imworkingnow@msn.com>
2019-09-30 22:42:00 -0700

    added tag poligon

--- src/index.js
+++ src/index.js
@@ -11,41 +11,46 @@ import Alert from 'bootstrap/js/dist/alert.js';
 
 import VisualizerBase from '@selia/visualizer';
 
-
 const visualizer = new VisualizerBase({
   canvas: document.getElementById('dummyCanvas'),
   itemInfo: {},
   active: false,
 });
 
+// Quita el menu contextual
+$('#annotatorCanvas').bind('contextmenu', function(e) {
+  return false;
+});
 
 function registerAnnotation(annotation) {
   let parent = document.getElementById('alert-container');
 
   let alert_div = document.createElement('div');
-  alert_div.className = "alert alert-primary";
+  alert_div.className = 'alert alert-primary';
   alert_div.innerHTML = `Annotation: ${JSON.stringify(annotation)}`;
 
   alert_div.setAttribute('role', 'alert');
-  alert_div.setAttribute('style', 'position: absolute; z-index:99; opacity: 0.9; display: none;');
+  alert_div.setAttribute(
+    'style',
+    'position: absolute; z-index:99; opacity: 0.9; display: none;',
+  );
 
   parent.appendChild(alert_div);
   let alert = new Alert(alert_div);
 
-  $('.alert').fadeIn('fast')
+  $('.alert').fadeIn('fast');
   setTimeout(() => {
-    $('.alert').fadeOut('fast', () => alert.close())
+    $('.alert').fadeOut('fast', () => alert.close());
   }, 1000);
 }
 
-
-import(/* webpackIgnore: true */'/annotator.js').then(module => {
+import(/* webpackIgnore: true */ '/annotator.js').then(module => {
   var config = {
     canvas: document.getElementById('annotatorCanvas'),
     visualizer: visualizer,
     edit: true,
     registerAnnotation: registerAnnotation,
-  }
+  };
 
   var annotator = new Annotator.default(config);
 });
